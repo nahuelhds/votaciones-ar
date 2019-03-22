1823ce3db26c203e12bdef0a3a5852f77513106d
Nahue <nsotelo@cristalmedia.net>
2019-03-22 13:32:58 -0300

    Voting API Resource ahora que ya no se encarga de importar

--- app/Http/Controllers/API/v1/VotingController.php
+++ app/Http/Controllers/API/v1/VotingController.php
@@ -16,9 +16,6 @@ use App\VotingVote;
 
 class VotingController extends Controller
 {
-    const CHAMBER_DEPUTIES = 'deputies';
-    const CHAMBER_SENATORS = 'senators';
-
     /**
      * Display a listing of the resource.
      *
@@ -37,79 +34,18 @@ class VotingController extends Controller
      */
     public function store(Request $request)
     {
-        $votedAt = Carbon::parse($request->voted_at);
-        $voting = Voting::firstOrNew([
-            'chamber' => $request->chamber,
-            'voted_at' => $votedAt,
-            'title' => $request->title
-        ]);
+        $voting = new Voting();
 
+        $voting->chamber = $request->chamber;
+        $voting->voted_at = Carbon::parse($request->voted_at);
+        $voting->title = $request->title;
         $voting->period = $request->period;
         $voting->meeting = $request->meeting;
         $voting->record = $request->record;
         $voting->type = $request->type;
         $voting->president_id = $request->president_id;
         $voting->result = $request->result;
-        $voting->source_url = $request->source_url;
-        $voting->original_id = $request->original_id;
-
         $voting->save();
-
-        if ($request->has('records')) {
-            foreach ($request->records as $record) {
-                VotingRecord::firstOrCreate([
-                    'voting_id' => $voting->id,
-                    'title' => $record['title'],
-                    'original_id' => $record['original_id'],
-                ]);
-            }
-        }
-
-        if ($request->has('detailsCsv')) {
-            $detailsCsv = $request->detailsCsv;
-            $rows = str_getcsv($detailsCsv, "\n");
-            array_shift($rows);
-            foreach ($rows as $row) {
-                $columns = str_getcsv($row);
-                $region = Region::firstOrCreate([
-                    'name' => trim($columns[3]),
-                ]);
-                $party = Party::firstOrCreate([
-                    'name' => trim($columns[2]),
-                ]);
-                $legislator = Legislator::firstOrNew([
-                    'name' => trim($columns[1]),
-                    'last_name' => trim($columns[0])
-                ]);
-
-                $legislator->type = Legislator::TYPE_DEPUTY;
-                $legislator->party_id = $party->id;
-                $legislator->region_id = $region->id;
-                $legislator->save();
-
-                switch ($columns[4]) {
-                    case 'AFIRMATIVO':
-                        $vote = VotingVote::VOTE_AFFIRMATIVE;
-                        break;
-                    case 'NEGATIVO':
-                        $vote = VotingVote::VOTE_NEGATIVE;
-                        break;
-                    case 'ABSTENCION':
-                        $vote = VotingVote::VOTE_ABSTENTION;
-                        break;
-                    default:
-                        $vote = null;
-                }
-
-                VotingVote::firstOrCreate([
-                    'voting_id' => $voting->id,
-                    'legislator_id' => $legislator->id,
-                    'party_id' => $party->id,
-                    'region_id' => $region->id,
-                    'vote' => $vote,
-                ]);
-            }
-        }
 
         return $voting;
     }
@@ -134,6 +70,7 @@ class VotingController extends Controller
      */
     public function update(Request $request, Voting $voting)
     {
+        $voting->chamber = $request->chamber;
         $voting->type = $request->type;
         $voting->voted_at = $request->voted_at;
         $voting->period = $request->period;
@@ -143,12 +80,9 @@ class VotingController extends Controller
         $voting->voting_type = $request->voting_type;
         $voting->president_id = $request->president_id;
         $voting->result = $request->result;
-        $voting->source_url = $request->source_url;
-        $voting->original_id = $request->original_id;
-
         $voting->save();
 
-        return new VotingCollection($voting);
+        return $voting;
     }
 
     /**
@@ -160,6 +94,6 @@ class VotingController extends Controller
     public function destroy(Voting $voting)
     {
         $voting->delete();
-        return new VotingCollection($voting);
+        return $voting;
     }
 }
