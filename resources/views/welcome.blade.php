e8c1cb3a1154da50d6586cfdad16dcfd8a8e865b
Nahue <nsotelo@cristalmedia.net>
2019-07-13 23:11:54 -0300

    Landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,25 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Laravel</title>
+        <title>Cómo votó</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+            rel="stylesheet"
+        />
+        <script src="https://kit.fontawesome.com/8696727dc8.js"></script>
 
         <!-- Styles -->
         <style>
-            html, body {
+            html,
+            body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: "Nunito", sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -48,12 +53,16 @@
                 font-size: 84px;
             }
 
+            .subtitle {
+                font-size: 32px;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
-                letter-spacing: .1rem;
+                letter-spacing: 0.1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
@@ -66,32 +75,39 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+            <div class="top-right links">
+                @auth
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+                @endif @endauth
+            </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Cómo votó 🇦🇷
                 </div>
-
+                <div class="subtitle m-b-md">
+                    <p>
+                        Próximamente vas a poder ver cómo votan nuestros
+                        representantes
+                    </p>
+                </div>
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a
+                        href="https://github.com/nahuelhds/votaciones-ar-datasets"
+                        ><i class="fas fa-file-excel fa-2x"></i> Datasets</a
+                    >
+                    <a href="https://github.com/nahuelhds/votaciones-ar">
+                        <i class="fab fa-github fa-2x"></i> GitHub</a
+                    >
+                    <a href="https://twitter.com/nahuelhds"
+                        ><i class="fab fa-twitter fa-2x"></i> Twitter</a
+                    >
                 </div>
             </div>
         </div>
