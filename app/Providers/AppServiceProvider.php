6f93d15f9f2b1368a42e5f7b3d83b343fc347f79
Nahue <nsotelo@cristalmedia.net>
2019-07-21 23:31:46 -0300

    Fix: SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // See: https://laravel.com/docs/master/migrations#creating-indexes
+        // Avoids
+        // "SQLSTATE[42000]: Syntax error or access violation: 1071
+        // Specified key was too long; max key length is 767 bytes"
+        Schema::defaultStringLength(191);
     }
 }
